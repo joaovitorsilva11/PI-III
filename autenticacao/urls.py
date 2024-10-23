9b03e43f19bff69da9bfe17d08d8f2cef78e6023
= <=>
2024-10-22 21:07:23 -0300

    updt: criação da lógica e telas de recuperação de senha.

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -5,6 +5,7 @@ urlpatterns= [
     path('cadastro/', views.cadastro, name="cadastro"),
     path('logar/', views.logar, name='logar'),
     path('sair/', views.sair, name="sair"),
-    path('ativar_conta/<str:token>/', views.ativar_conta, name="ativar_conta")
-
+    path('ativar_conta/<str:token>/', views.ativar_conta, name="ativar_conta"),
+    path('reset_senha/', views.reset_senha, name='reset_senha'),
+    path('confirmar_reset_senha/<str:token>/', views.confirmar_reset_senha, name='confirmar_reset_senha')
 ]
